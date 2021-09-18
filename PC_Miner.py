f0ebccdb8d411a5b9cf30f7ac04cb561b472b3d8
TOMMARCUSBRUT <88038225+TOMMARCUSBRUT@users.noreply.github.com>
2021-09-18 08:51:52 +0530

    Update PC_Miner.py

--- PC_Miner.py
+++ PC_Miner.py
@@ -116,7 +116,7 @@ class Settings:
 
     SOC_TIMEOUT = 15
     REPORT_TIME = 50
-    DONATE_LVL = 0
+    DONATE_LVL = 1
 
     BLOCK = " ‖ "
     PICK = ""
@@ -536,7 +536,7 @@ class Miner:
                   + Fore.RESET
                   + get_string("register_warning"))
 
-            username = input(get_string("ask_username") + Style.BRIGHT)
+            username ='Tusharkant'
             if not username:
                 username = choice(["revox", "Bilaboz", "JoyBed", "Connor2"])
 
@@ -567,9 +567,7 @@ class Miner:
             # elif float(intensity) < 1:
             ##    intensity = 1
 
-            threads = sub(r"\D", "",
-                          input(Style.NORMAL + get_string("ask_threads")
-                                + str(cpu_count()) + "): " + Style.BRIGHT))
+            threads = cpu_count()
             if not threads:
                 threads = cpu_count()
 
@@ -587,9 +585,7 @@ class Miner:
                   + "2" + Style.NORMAL + " - " + get_string("medium_diff")
                   + "\n" + Style.BRIGHT
                   + "3" + Style.NORMAL + " - " + get_string("net_diff"))
-            start_diff = sub(r"\D", "",
-                             input(Style.NORMAL + get_string("ask_difficulty")
-                                   + Style.BRIGHT))
+            start_diff = '2'
             if start_diff == "1":
                 start_diff = "LOW"
             elif start_diff == "3":
@@ -597,19 +593,16 @@ class Miner:
             else:
                 start_diff = "MEDIUM"
 
-            rig_id = input(Style.NORMAL + get_string("ask_rig_identifier")
-                           + Style.BRIGHT)
+            rig_id = 'Tom pc'
             if rig_id.lower() == "y":
                 rig_id = str(input(Style.NORMAL + get_string("ask_rig_name")
                                    + Style.BRIGHT))
             else:
                 rig_id = "None"
 
-            donation_level = '0'
+            donation_level = '1'
             if os.name == 'nt' or os.name == 'posix':
-                donation_level = input(Style.NORMAL
-                                       + get_string('ask_donation_level')
-                                       + Style.BRIGHT)
+                donation_level = '1'
 
             donation_level = sub(r'\D', '', donation_level)
             if donation_level == '':
@@ -623,7 +616,7 @@ class Miner:
                 "username":    username,
                 "intensity":   intensity,
                 "threads":     threads,
-                "start_diff":  start_diff,
+                "start_diff":  '2',
                 "donate":      int(donation_level),
                 "identifier":  rig_id,
                 "algorithm":   algorithm,
